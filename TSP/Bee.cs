df365cd3deb2d0413ed9de437fffd6d7b8c9741e
Furkan <falniak95@gmail.com>
2021-12-25 00:40:41 +0300

    Project uploaded

---
+++ TSP/Bee.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace TSP
+{
+    class Bee
+    {
+        public int status;
+        public PointF[] memoryPath;
+        public double measureOfQuality;
+        public int numberOfVisits;
+
+        public Bee(int stat, PointF[] path, double measure, int numVisits)
+        {
+            this.status = stat;
+            this.memoryPath = path;
+            this.measureOfQuality = measure;
+            this.numberOfVisits = numVisits;
+        }
+    }
+}
